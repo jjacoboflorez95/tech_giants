db578e72e30b621759cebff187eb4370b4fae7f2
jjacoboflorez95 <jjacoboflorez95@gmail.com>
2023-08-08 11:59:34 -0400

    Removal of unnecessary comments

--- public/js/changeFontFamilyWebsite.js
+++ public/js/changeFontFamilyWebsite.js
@@ -5,7 +5,7 @@ changeFontFamily.addEventListener("click", () => {
 	// Get the computed style of the element
 	const computedStyle = window.getComputedStyle(document.body);
 	// Get the value of the font-family property
-	let fontFamily = computedStyle.fontFamily;
+	const fontFamily = computedStyle.fontFamily;
 
 	//console.log("Font Family before change:", fontFamily);
 	if (
@@ -13,19 +13,8 @@ changeFontFamily.addEventListener("click", () => {
 		'"Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif'
 	) {
 		document.body.style.fontFamily = "font-family";
-		/*fontFamily = "font-family";*/
 	} else {
 		document.body.style.fontFamily =
 			'"Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif';
-		/*fontFamily =
-			'"Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial, sans-serif';*/
 	}
-
-	/*fetch("/update_font_website", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ fontFamily: fontFamily }),
-	});*/
 });
